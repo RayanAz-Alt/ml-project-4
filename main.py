0fbf9939200c437cafbe498a990a661d2ea81594
Rayan Azmatally <rayan.azmatally@gmail.com>
2024-10-22 15:03:18 +0100

    fix: files names

--- main.py
+++ main.py
@@ -4,3 +4,20 @@ import numpy as np
 import tensorflow as tf
 
 
+X_train2_a = np.load("Xtrain2_a.npy")
+X_train2_b = np.load("Xtrain2_b.npy")
+
+Y_train2_a = np.load("Ytrain2_a.npy")
+Y_train2_b = np.load("Ytrain2_b.npy")
+
+X_test2_a = np.load("Xtest2_a.npy")
+X_test2_b = np.load("Xtest2_b.npy")
+
+print(X_train2_a.shape)
+print(X_train2_b.shape)
+
+print(Y_train2_a.shape)
+print(Y_train2_b.shape)
+
+print(X_test2_a.shape)
+print(X_test2_b.shape)
